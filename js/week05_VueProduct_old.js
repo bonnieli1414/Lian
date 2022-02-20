a776848fe561e07a434b16f950c6ce298a628e83
Lian <bonnieli1414@gmail.com>
2022-02-21 02:38:35 +0800

    將老師講解改為old，更新助教講解的第5週任務作業

--- js/week05_VueProduct_old.js
+++ js/week05_VueProduct_old.js
@@ -2,33 +2,10 @@
 
 import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js";
 
-// 宣告
-// Form, Field, ErrorMessage 元件
-// defineRule, configure 函式
-const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
-const { required, email, min, max } = VeeValidateRules;
-// 引入多國語系的函式
-const { localize, loadLocaleFromURL } = VeeValidateI18n;
-
-// VeeValidate提供的函式，用來定義規則
-// 只載入需要使用的
-defineRule('required', required);
-defineRule('email', email);
-defineRule('min', min);
-defineRule('max', max);
-// 宣告
-loadLocaleFromURL('https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json');
-
-configure({ // 用來做一些設定
-  //啟用 locale，設定語系
-  generateMessage: localize('zh_TW'), 
-});
-
 const apiUrl = "https://vue3-course-api.hexschool.io/v2";
 const apiPath = "bonnieli1414";
 
-// 全域 root
-Vue.createApp({
+const app = createApp({
   data() {
     return {
       cartData: {},
@@ -36,27 +13,7 @@ Vue.createApp({
       productId: '',
       // 局部讀取效果
       isLoadingItem:'',
-      form: {
-        user: {
-          name: '',
-          email: '',
-          tel: '',
-          address: '',
-        },
-        message: '',
-      },
-      cart: {
-        // 要先設定 carts 為空陣列，否則會噴錯
-        // 亦可設定串連運算子，
-        carts:[],
-      },
     };
-  },
-  // VeeValiadation 使用*區域元件*的載入方式
-  components: {
-    VForm: Form,
-    VField: Field,
-    ErrorMessage: ErrorMessage,
   },
   methods: {
     getProducts() {
@@ -94,24 +51,13 @@ Vue.createApp({
         this.isLoadingItem = '';
       });
     },
-    // 清空單項產品
-    removeCartItem(id){      
+    removeCartItem(id){
       this.isLoadingItem = 'id';
       axios.delete(`${apiUrl}/api/${apiPath}/cart/${id}`)
       .then((res)=>{
         console.log(res);        
         this.getCart(); 
         this.isLoadingItem = '';
-      });
-    },
-    // 刪除全部品項
-    deleteAllCarts() {
-      const url = `${apiUrl}/api/${apiPath}/carts`;
-      axios.delete(url).then((response) => {
-        alert(response.data.message);
-        this.getCart();
-      }).catch((err) => {
-        alert(err.data.message);
       });
     },
     updateCartItem(item){
@@ -128,28 +74,15 @@ Vue.createApp({
         this.isLoadingItem = '';
       });
     },
-    // 客戶填寫訂單
-    createOrder() {
-      const url = `${apiUrl}/api/${apiPath}/order`;
-      const order = this.form;
-      axios.post(url, { data: order }).then((response) => {
-        alert(response.data.message);
-        // 用 vee validate 的方法 resetForm方法清空欄位
-        this.$refs.form.resetForm();
-        this.getCart();
-      }).catch((err) => {
-        alert(err.data.message);
-      });
-    },
   },
   mounted() {
     this.getProducts();
     this.getCart();
   },
-})
+});
 
 // $refs
-.component('product-modal',{
+app.component('product-modal',{
   props:['id'],
   template:'#userProductModal',
   data(){
@@ -198,7 +131,6 @@ Vue.createApp({
     // 將 modal.show() 移動至 methods ，並改成 this.modal
     this.modal = new bootstrap.Modal(this.$refs.modal); 
   }
-})
+});
 
-//根實體，並掛載於#app
-.mount("#app");
+app.mount("#app");
