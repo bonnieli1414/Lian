3fa003eef1105f905bfca48403eae5e45fab4a31
Lian <bonnieli1414@gmail.com>
2022-02-28 19:38:49 +0800

    將第5週作業購物車的html和js貼上

--- week6_class/src/views/CartView.vue
+++ week6_class/src/views/CartView.vue
@@ -1,3 +1,103 @@
 <template>
-    <h2>購物車</h2>
+    <div class="container">
+      <h2>購物車</h2>
+      <table class="table align-middle">
+        <thead>
+          <tr>
+            <th>圖片</th>
+            <th>商品名稱</th>
+            <th>價格</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products" :key="product.id">
+            <td style="width: 200px">
+              <div
+                :style="{
+                  backgroundImage: `url(${product.imageUrl})`,
+                }"
+                style="height: 100px; background-size: cover; background-position: center"
+              ></div>
+            </td>
+            <td>{{ product.title }}</td>
+            <td>
+              <div class="h5" v-if="product.price === product.origin_price">
+                {{ product.price }} 元
+              </div>
+              <div v-else>
+                <del class="h6">原價 {{ product.origin_price }} 元</del>
+                <div class="h5">現在只要 {{ product.price }} 元</div>
+              </div>
+            </td>
+            <td>
+              <div class="btn-group btn-group-sm">
+                <button
+                  type="button"
+                  class="btn btn-outline-secondary"
+                  :disabled="isLoadingItem === product.id"
+                  @click="openProductModal(product.id)"
+                >
+                  查看更多
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-danger"
+                  @click="addToCart(product.id)"
+                  :disabled="isLoadingItem === product.id"
+                >
+                  <span
+                    class="spinner-grow spinner-grow-sm"
+                    v-show="isLoadingItem === product.id"
+                  ></span>
+                  加到購物車
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- 購物車列表 -->
+    </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      cartData: {},
+      products: [],
+      isLoadingItem: ''
+    }
+  },
+  methods: {
+    getProducts () {
+      this.$http(`${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products/all`)
+        .then((res) => {
+        // console.log(res);
+          this.products = res.data.products
+        })
+    },
+    // 畫面沒有調整數量的地方，所以這裡設定參數預設值
+    addToCart (id, qty = 1) {
+      const data = {
+        product_id: id,
+        qty
+      }
+      this.isLoadingItem = id
+      this.$http.post(`${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart`, { data })
+        .then((res) => {
+          console.log(res)
+          // 加入購物車後，再重新取得購物車資料
+          this.getCart()
+          this.$refs.productModal.closeModal()
+          this.isLoadingItem = ''
+        })
+    }
+  },
+  mounted () {
+    this.getProducts()
+  }
+}
+
+</script>
