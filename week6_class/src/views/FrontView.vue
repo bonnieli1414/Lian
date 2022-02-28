89e43f59b1396d7b875e53c66c4435b52d0ee011
Lian <bonnieli1414@gmail.com>
2022-02-28 19:09:08 +0800

    封裝元件

--- week6_class/src/views/FrontView.vue
+++ week6_class/src/views/FrontView.vue
@@ -1,10 +1,12 @@
 <template>
+<!-- 將navbar封裝拆成元件存至components =>FrontNavbar.vue檔案，並用匯入的型式會進來 -->
     <FrontNavbar></FrontNavbar>
     <router-view></router-view>
 </template>
 <script>
 import FrontNavbar from '@/components/FrontNavbar.vue'
 
+// 匯入元件 =>FrontNavbar.vue檔案
 export default {
   components: {
     FrontNavbar
