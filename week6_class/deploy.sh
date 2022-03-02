1d0312230ad6233a0ecf88b07ad92fe9b01c3053
Lian <bonnieli1414@gmail.com>
2022-03-02 20:56:39 +0800

    更正Cli部屬

--- week6_class/deploy.sh
+++ week6_class/deploy.sh
@@ -1,30 +1,39 @@
-#!/usr/bin/env sh
-
-# 當發生錯誤時終止
+# 發生錯誤時執行終止指令
 set -e
 
-# 建構 (build這裡做的就是幫我們把Vue轉成HTML/JS/CSS，並放入dist)
+# 取得目前專案的遠端分支
+originUrl=$(git config --get remote.origin.url)
+
+# 注意若是出現以下 Url 就是走 SSH
+# git@github.com:hsiangfeng/HexfootMusic.git
+# 反之，若是 HTTPs 的話則是以下
+# https://github.com/hsiangfeng/HexfootMusic.git
+# 通常來講走 SSH 可能會出現 GitHub Public Keys 錯誤，這部分 Google 一下就可以解決囉。
+
+nowStatus=${originUrl:0:5}
+echo $nowStatus
+if [ $nowStatus = 'https' ]
+then
+  echo '傳輸方式採用 HTTPs 模式'
+  echo '目前遠端分支 URL：'$originUrl
+else
+  echo '傳輸方式採用 SSH 模式'
+  echo '目前遠端分支 URL：'$originUrl
+fi
+
+# 打包編譯
 npm run build
 
-# save the lastest commit hash as a string
-LOGSTRING=$(git log)
-COMMIT=$(echo $LOGSTRING | awk '{print $2}')
-
-# cd 到建構輸出的目錄下
+echo '移動目錄到編譯出來的 dist 資料夾'
+#移動到打包資料夾下，若你有調整的話打包後的資料夾請務必調整
 cd dist
-
-# 如果要部署到自定義域名
-# echo 'www.example.com' > CNAME
-
+# 初始化並設置 Git
 git init
-git add -A //加入整個dist
-git commit -m "deploy (commit: $COMMIT)"
+git add .
+git commit -m "update `date +'%Y-%m-%d %H:%M:%S'`";
 
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# 上傳到到 gh-pages
+# 如果是走 SSH 的話可能會比較不好調整
+git push -f $originUrl master:gh-pages
 
-# 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:bonnieli1414/Lian.git master:gh-pages
-
-cd ..
+cd -
